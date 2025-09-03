9a37d4eacdc6bc03ed968fc861ddf24c47f68f19
christian figueiredo <developer.figueiredo@gmail.com>
2025-09-03 17:27:46 -0300

    add context and migrations

---
+++ Back/src/ProEventos.API/Data/Contexto.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Data
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Evento> Eventos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
